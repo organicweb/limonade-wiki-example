25e5beeedb4ef2bf517c85c01736fd4d7cbbdd9a
Fabrice Luraine <fabrice.luraine@sofa-design.net>
2009-06-03 19:59:29 +0200

    Fixing form action

--- views/new.php
+++ views/new.php
@@ -1,5 +1,5 @@
 <h1>Create page: <?=h($page_name)?></h1>
-<form action="/create" method="post">
+<form action="<?=url_for('create')?>" method="post">
   <input type="hidden" name="page_name" value="<?=page_name?>" id="page_name">
   <textarea name="page_content" id="page_content" rows="8" cols="40"></textarea>
   <p><input type="submit" value="Create &rarr;"></p>
