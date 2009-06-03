4341b17ecb989b68ede19cd24f1d78c426f74956
Fabrice Luraine <fabrice.luraine@sofa-design.net>
2009-06-03 19:59:40 +0200

    Fixing and adding magic _method hidden field

--- views/edit.php
+++ views/edit.php
@@ -1,5 +1,6 @@
 <h1>Edit page: <?=h($page_name)?></h1>
-<form action="/create" method="post">
+<form action="<?=url_for($page_name)?>" method="post">
+  <input type="hidden" name="_method" value="PUT" id="_method">
   <input type="hidden" name="page_name" value="<?=page_name?>" id="page_name">
   <textarea name="page_content" id="page_content" rows="8" cols="40"><?=$page_content;?></textarea>
   <p><input type="submit" value="Update &rarr;"></p>
