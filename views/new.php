ba31908580df4333edf881e2f760243dc1e078e6
Fabrice Luraine <fabrice.luraine@sofa-design.net>
2009-06-03 17:39:48 +0200

    Adding basic views

---
+++ views/new.php
@@ -0,0 +1,6 @@
+<h1>Create page: <?=h($page_name)?></h1>
+<form action="/create" method="post">
+  <input type="hidden" name="page_name" value="<?=page_name?>" id="page_name">
+  <textarea name="page_content" id="page_content" rows="8" cols="40"></textarea>
+  <p><input type="submit" value="Create &rarr;"></p>
+</form>
---
+++ views/show.php
@@ -0,0 +1,4 @@
+<h1><?=h($page_name);?></h1>
+<div id="page_content">
+  <?=wikir_render($page_content);?>
+</div>
