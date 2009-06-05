9fba17c73dbd9639c1fe7029d4f6eabed8cbde24
Fabrice Luraine <fabrice.luraine@sofa-design.net>
2009-06-05 10:47:29 +0200

    add delete link to show view

--- views/show.php
+++ views/show.php
@@ -2,3 +2,11 @@
 <div id="page_content">
   <?=wikir_render($page_content);?>
 </div>
+<div id="buttons">
+  <p>
+    <a href="<?=url_for($page_name, 'edit');?>">Edit</a>
+    <? if($page_name != 'Home'): ?>
+    | <a href="<?=url_for($page_name);?>" onclick="if (confirm('Are you sure?')) { var f = document.createElement('form'); f.style.display = 'none'; this.parentNode.appendChild(f); f.method = 'POST'; f.action = this.href;var m = document.createElement('input'); m.setAttribute('type', 'hidden'); m.setAttribute('name', '_method'); m.setAttribute('value', 'DELETE'); f.appendChild(m); f.submit(); };return false;">Delete</a>
+    <? endif; ?>
+  </p>
+</div>
