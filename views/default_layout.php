395af9876f700d46fc2f5485103b08cb0096142e
Mathias Standaert <contact@organicweb.fr>
2009-06-11 12:21:34 +0200

    Added css sheets Added div#header, div#content, div#footer

--- views/default_layout.php
+++ views/default_layout.php
@@ -7,11 +7,25 @@
 	<title>Wikir</title>
 	<meta name="generator" content="TextMate http://macromates.com/">
 	<meta name="author" content="Fabrice Luraine">
+	<link rel="stylesheet" href="http://www.sofa-design.net/limonade/css/blueprint/screen.css" type="text/css" media="screen, projection">
+  	<link rel="stylesheet" href="http://www.sofa-design.net/limonade/css/blueprint/print.css" type="text/css" media="print"> 
+  <!--[if IE]>
+    <link rel="stylesheet" href="http://www.sofa-design.net/limonade/css/blueprint/ie.css" type="text/css" media="screen, projection">
+  <![endif]-->
+  <link rel="stylesheet" href="http://www.sofa-design.net/limonade/css/screen.css" type="text/css" media="screen, projection">
 	<!-- Date: 2009-06-03 -->
 </head>
 <body>
-
-<?= $content; ?>
+<div id="header">
+    <h1>Limonade</h1>
+    <p>a PHP micro-framework</p>
+  </div>
+  <div id="content">
+	<?= $content; ?>
+	</div>
+	<div id="footer">
+		&nbsp;
+	</div>
 
 </body>
 </html>
